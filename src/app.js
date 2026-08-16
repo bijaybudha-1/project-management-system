e4aa18abad976558e613647ce5217d243531e861
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-16 22:53:22 +0545

    CORS Installation and Configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -17,6 +18,15 @@ app.use(
 );
 
 app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN.split(", ") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my Project Management System!!");
