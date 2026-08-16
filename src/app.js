7b048ca2243d8a41a5f325173ca36c69f13df4cd
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-16 22:46:05 +0545

    server and express configuration

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+// Base Configuration
+app.use(
+  express.json({
+    limit: "16kb",
+  }),
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  }),
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Project Management System!!");
+});
+
+export default app;
