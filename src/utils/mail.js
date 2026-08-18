bd7b0ca02257eb75c154bbfd77777e37f81756c1
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-18 15:29:11 +0545

    Setup forget password mailgen content and export both mailgen features

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -6,7 +6,7 @@ const emailVerificationMailgenContent = (username, verificationURL) => {
       name: username,
       intro: "Welcome to our App! we'are excited to have on board.",
       action: {
-        instruction:
+        instructions:
           "To verify your email please click on the following button.",
         button: {
           color: "#22BC66",
@@ -19,3 +19,24 @@ const emailVerificationMailgenContent = (username, verificationURL) => {
     },
   };
 };
+
+const forgotPasswordMailgenContent = (username, passwordResetURL) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link",
+        button: {
+          color: "#22BC66",
+          text: "Reset Password",
+        },
+      },
+      outro:
+        "Need help or have question? Just reply to this email, we'd love to help",
+    },
+  };
+};
+
+export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
