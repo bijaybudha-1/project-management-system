0a1f63cfa096effad476822310529376ac98d43b
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-19 16:32:07 +0545

    Add and export userResgisterValidator

---
+++ src/validators/index.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase("Username must be in lower case")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 character long"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 character long"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
