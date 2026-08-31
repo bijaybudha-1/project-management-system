497a9f4c1ce5be12a25b8023d87c30ba0677a749
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-31 22:59:45 +0545

    Add Task modeles

---
+++ src/models/task.models.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatus, TaskStatusEnum } from "../utils/constants.js";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    assignedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: AvailableTaskStatus,
+      default: TaskStatusEnum.TODO,
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          mimeType: String,
+          size: Number,
+        },
+      ],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Task = mongoose.model("Task", taskSchema);
