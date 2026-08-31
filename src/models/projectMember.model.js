1779909ec6c5ec41ff81af372657231def0a83e3
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-31 22:52:00 +0545

    Add ProjectMember modeles

---
+++ src/models/projectMember.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableUserRole, userRolesEnum } from "../utils/constants.js";
+
+const projectMemberSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: AvailableUserRole,
+      default: userRolesEnum.MEMBER,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
