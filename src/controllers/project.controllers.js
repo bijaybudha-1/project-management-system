0efda59c88b8a6292c462004ae93f49adf4f05ce
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-09-01 16:30:26 +0545

    Add updateProject

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -4,6 +4,7 @@ import { ProjectMember } from "../models/projectMember.model.js";
 import mongoose from "mongoose";
 import { userRolesEnum } from "../utils/constants.js";
 import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
 
 const createProject = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
@@ -23,4 +24,26 @@ const createProject = asyncHandler(async (req, res) => {
   return res
     .status(201)
     .json(new ApiResponse(201, project, "Project created successfully"));
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const { projectId } = req.params;
+
+  const project = await Project.findByIdAndUpdate(
+    projectId,
+    {
+      name,
+      description,
+    },
+    { new: true },
+  );
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project updated successfully"));
 });
